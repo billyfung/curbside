e5d3b9c4f2b3339ae5131672bd1019c5ab267cd0
Billy Fung <billy@billyfung.com>
2015-02-01 16:18:38 -0800

    almost done

--- curbside.py
+++ curbside.py
@@ -4,24 +4,28 @@ import json
 
 #search the tree to find greatest depth secrets
 #session needs to be re-done every time
-def search(start, depth, session):
-	opensession = urllib.request.Request('http://challenge.shopcurbside.com/%s' % start, headers={'Session':session})
+def search(s, depth, session):
+	opensession = urllib.request.Request('http://challenge.shopcurbside.com/%s' % s, headers={'Session':session})
 	try:
 		abc = urllib.request.urlopen(opensession)
-		a = abc.read()
+		s = abc.read()
 		abc.close()
-		session = urllib.request.urlopen('http://challenge.shopcurbside.com/get-session').read().decode('utf-8')
 	except:
-		print('Error: depth %d %s' %(depth,a))
+		print('Error: depth %d %s' %(depth,s))
 		return
-	secret = json.loads(a.decode('utf-8'))
+	secret = json.loads(s.decode('utf-8'))
 	if 'message' in secret:
 		print(secret['message'])
 	if 'next' in secret:
 		for i in secret['next']:
+			session = sessionrefresh()
 			search(i, depth+1, session)
 	else:
 		print(secret)
+
+def sessionrefresh():
+	session = urllib.request.urlopen('http://challenge.shopcurbside.com/get-session').read().decode('utf-8')
+	return session
 
 session = urllib.request.urlopen('http://challenge.shopcurbside.com/get-session').read().decode('utf-8')
 search('start', 0, session)
